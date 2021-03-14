3977b78cb7ff4fed5e8054449d691901764b229a
Greta Tuckute <gretatu@mit.edu>
2021-03-14 18:21:24 -0400

    transcribe cfgs

--- transcribe.py
+++ transcribe.py
@@ -4,12 +4,32 @@ from hydra.core.config_store import ConfigStore
 from deepspeech_pytorch.configs.inference_config import TranscribeConfig
 from deepspeech_pytorch.inference import transcribe
 
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+
 cs = ConfigStore.instance()
 cs.store(name="config", node=TranscribeConfig)
+
+DATA_DIR = '/Users/gt/Documents/GitHub/asr/decoding/165_natural_sounds/165_natural_sounds/'
+
+files = [f for f in listdir(DATA_DIR) if isfile(join(DATA_DIR, f))]
+wav_files = [f for f in files if f.endswith('wav')]
 
 
 @hydra.main(config_name="config")
 def hydra_main(cfg: TranscribeConfig):
+    # run across several audio files
+    sorted_wav = np.sort(wav_files)
+
+    cfg_all = {}
+    for file in wav_files:
+        cfg_copy = cfg.copy()
+    
+        cfg_copy['audio_path'] = DATA_DIR + file
+    
+        cfg_all[file] = cfg_copy
+    
     transcribe(cfg=cfg)
 
 
